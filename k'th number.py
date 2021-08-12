1ca1078fd4777703ffe20e0b09d0d4e670cac751
parkchanbin54 <82207081+parkchanbin54@users.noreply.github.com>
2021-08-13 08:05:43 +0900

    Add files via upload

---
+++ k'th number.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[76]:
+
+
+def solution(array,commands):
+    answer=[]
+    for k in range (len(commands)):
+        s=array[commands[k][0]-1:commands[k][1]];
+        for i in range (len(s)):
+            for j in range (len(s)-1):
+                if s[j]>s[i]:
+                    tmp=s[j];
+                    s[j]=s[i];
+                    s[i]=tmp;
+        print(commands[k][2])
+        print(s)
+        answer.append(s[(commands[k][2])-1]);
+    return answer
+
+
+# In[77]:
+
+
+solution([1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]])
+
