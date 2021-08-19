2a19c2f8b52fb8cdeda871c4c2fcc13ba3d82a62
parkchanbin54 <82207081+parkchanbin54@users.noreply.github.com>
2021-08-19 19:46:25 +0900

    Add files via upload

---
+++ notification.java
@@ -0,0 +1,89 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.NotificationCompat.WearableExtender;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends Activity {
+
+    private ActivityMainBinding binding;
+    static final int BASIC_ID = 0;
+    static final String CHANNEL_ID = "channelId";
+    static String channel_name="channelId";
+    static String channel_description="hi";
+    Notification noti;
+    NotificationManagerCompat manager;
+
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+    }
+    public void sendNotification(View view) throws NoSuchPaddingException, InvalidAlgorithmParameterException, UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        createNotificationChannel();
+        String toSend = editText.getText().toString();
+        toSend=AES256.AES_Encode(toSend);
+        Log.d("encoded",toSend);
+        toSend=AES256.AES_Decode(toSend);
+        Log.d("decoded",toSend);
+        if(toSend.isEmpty())
+            toSend = "You sent an empty notification";
+        Notification notification = new NotificationCompat.Builder(getApplication(),CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("AndroidAuthority")
+                .setContentText(toSend)
+                .extend(new NotificationCompat.WearableExtender())
+                .build();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplication());
+        int notificationId = 1;
+        notificationManager.notify(notificationId, notification);
+    }
+    private void createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            // 채널에 필요한 정보 제공
+            CharSequence name = "test";
+            String description = "hi";
+
+            // 중요도 설정, Android7.1 이하는 다른 방식으로 지원한다.(위에서 설명)
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+}
