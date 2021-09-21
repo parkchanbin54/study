0c413198311fc1aface71eec09ef27e8a5a76311
parkchanbin54 <82207081+parkchanbin54@users.noreply.github.com>
2021-09-21 15:31:54 +0900

    Update AnalogComplicationConfigRecyclerViewAdapter.java

--- AnalogComplicationConfigRecyclerViewAdapter.java
+++ AnalogComplicationConfigRecyclerViewAdapter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+//
 package com.example.android.wearable.watchface.config;
 
 import static com.example.android.wearable.watchface.config.ColorSelectionActivity.EXTRA_SHARED_PREF;
