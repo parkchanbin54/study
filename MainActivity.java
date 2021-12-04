c4c5639de26e3e29f3b5a820f062bbdae993b56f
parkchanbin54 <82207081+parkchanbin54@users.noreply.github.com>
2021-12-05 07:29:50 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -17,7 +17,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+  
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
